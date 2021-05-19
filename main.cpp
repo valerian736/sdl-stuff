32350f9c34ada10d64d6a6c88e21bf5a714d66f2
valerian736 <valeriantendy@gmail.com>
2021-05-20 04:13:02 +0700

    hell0 mamamiya

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 using namespace sf;
 
 int main ()
 {
+  int points = 5;
+  bool display = true;
   ConvexShape convex;
-  convex.setPointCount(5);
+  convex.setPointCount(points);
   convex.setFillColor(Color::Green);
   RenderWindow win(VideoMode(1600, 800), "fuck you");
+  int x;
+  int y;
+
 
   while (win.isOpen())
   {
@@ -24,18 +30,44 @@ int main ()
         case Event::KeyPressed:
           if (Keyboard::isKeyPressed(Keyboard::A))
           {
+            display = true;
             convex.setPoint(0, sf::Vector2f(0.f, 0.f));
             convex.setPoint(1, sf::Vector2f(150.f, 10.f));
             convex.setPoint(2, sf::Vector2f(120.f, 90.f));
             convex.setPoint(3, sf::Vector2f(30.f, 100.f));
             convex.setPoint(4, sf::Vector2f(0.f, 50.f));
           }
+          if (ev.key.code == Keyboard::B)
+          {
+            display = false;
+          }
+          // if (Keyboard::isKeyPressed(Keyboard::B))
+          // {
+
+          // }
+          
           std::cout << "key:" << ev.key.code << std::endl;
+        case Event::MouseButtonPressed:
+          Vector2i pos = Mouse::getPosition(win);
+          x = pos.x;
+          y = pos.y;
+
+          if (Mouse::isButtonPressed(Mouse::Left))
+          {
+            
+              std::cout << "x:" << x << "y:" << y << std::endl;
+          }
+
 
         }
     }
     win.clear(Color::White);
-    win.draw(convex);
+    if (display)
+    {
+      win.draw(convex);
+    }
+    
+    
     win.display();
   }
 
