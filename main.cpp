9de618489f201119a66ddbb42fb02b6c82eaca6f
valerian736 <valeriantendy@gmail.com>
2021-05-19 18:32:15 +0700

    lalala

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <SFML/Window.hpp>
+using namespace sf;
 
 int main ()
 {
+  Window win(VideoMode(800, 600), "fuck you");
 
+  while (win.isOpen())
+  {
+    Event ev;
+    while(win.pollEvent(ev))
+    {
+      switch (ev.type)
+      {
+      case Event::Closed :
+        win.close();
+        break;
+
+      
+
+    }
+  }
+  
 }
